cd9d73017d4973320efbf63f55efe085fd7363db
qufocdz <qufo.playz@gmail.com>
2024-12-17 21:01:31 +0100

    przed sroda+mypart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,10 +1,13 @@
 library globals;
 
-
-Map<String,dynamic>? user;
+const MONGO_URL =
+    "mongodb+srv://272796:drukomatdawid@users.tmgbk.mongodb.net/test?retryWrites=true&w=majority";
+const COLLECTION_USER = "User";
+const COLLECTION_DRUKOMAT = "drukomats";
 bool loggedIn = false;
 const int richBlack = 0xFF001011;
 const int midnightGreen = 0xFF093A3E;
 const int verdigris = 0xFF3AAFB9;
 const int electricBlue = 0xFF64E9EE;
 const int lightSkyblue = 0xFF97C8EB;
+const int beige = 0xFFF4F4F0;
